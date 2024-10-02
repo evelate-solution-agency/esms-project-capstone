2a86f98b9105e8317a83b1aa3ed957962b2601d3
Jomari <109713343+Noyzki@users.noreply.github.com>
2024-10-03 05:05:11 +0800

    added template view function for date_time.html

--- apps/core/views.py
+++ apps/core/views.py
@@ -74,3 +74,11 @@ class BadmintonView(TemplateView):
         context = TemplateLayout.init(self, super().get_context_data(**kwargs))
 
         return context
+
+class DateTimeView(TemplateView):
+    # Predefined function
+    def get_context_data(self, **kwargs):
+        # A function to init the global layout. It is defined in web_project/__init__.py file
+        context = TemplateLayout.init(self, super().get_context_data(**kwargs))
+
+        return context
